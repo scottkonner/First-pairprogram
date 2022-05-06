3c9cf1aa9e06c264b21b78f7fcb1e4cae797a200
Scott <scott_koner@yahoo.com>
2022-05-05 20:27:44 -0400

    problem 5 solved

--- problems/05-does-key-exist.js
+++ problems/05-does-key-exist.js
@@ -5,14 +5,23 @@ key is not inside the object.
 
 Examples:
 
-let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
-doesKeyExist(obj1, 'course'); // => true
-doesKeyExist(obj1, 'name'); // => false
+// Function will take in an OBJECT and a KEY
+// Function will return TRUE if KEY exists in the OBJECT
+// else return FALSE
+//Outline: Use an 'if' statement to get boolean output
+//Outline: To determine whether it exists, we should check if KEY is 'undefined' or not
 ***********************************************************************/
 
 function doesKeyExist(obj, key) {
-  // your code here
+  if (obj[key] !== undefined) {
+    return true
+  }
+  return false
 }
+
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+doesKeyExist(obj1, 'course'); // => true
+doesKeyExist(obj1, 'name'); // => false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = doesKeyExist;
