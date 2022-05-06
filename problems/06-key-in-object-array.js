dd3ce16172d46f6f18ecf0394c3edf61e693b794
Nicholas Hudson <Nicholash266@icloud.com>
2022-05-05 17:51:01 -0700

    prob 6 partial

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -1,27 +1,43 @@
 /***********************************************************************
-Write a function `keyInObjectArray(objArray, keyString)` that takes in an array 
-of objects as the first parameter and a string as the second. The 
-`keyInObjectArray` function will return `true` if any of the objects contains 
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array
+of objects as the first parameter and a string as the second. The
+`keyInObjectArray` function will return `true` if any of the objects contains
 the `keyString` as a key within them, and `false` if not.
 
+//understand: the function should determine if the string is witihn the object array
+//returns a BOOLEAN
+//FOR LOOP through the object array
+//use an if statement to get the BOOLEAN
+//if an objectkey in arr === string, return true
+//set a variable as objects.key(objArray) for object array.
+//that pulls keys ^ if the key === keystring return true
 
-Examples:
+***********************************************************************/
+
 let objArray = [
   { name: "Rupert" },
   { age: 42 },
   { planet: "Earth", system: "Milky Way" }
 ];
 
+
+
+function keyInObjectArray(objArray, keyString) {
+ // let keys = Object.keys(objArray);
+for (key of objArray){
+  
+}
+
+}
+
+
+Examples:
+
+
 keyInObjectArray(objArray, 'planet'); // => true
 keyInObjectArray(objArray, 'age'); // => true
 keyInObjectArray(objArray, 'food'); // => false
 keyInObjectArray(objArray, 'animal'); // => false
-
-***********************************************************************/
-
-function keyInObjectArray(objArray, keyString) {
-  // your code here
-}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
